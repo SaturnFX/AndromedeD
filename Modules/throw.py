2992040b94734eee43dac9d3a95553fa8f3b7340
SaturnFX <saturniak8888@gmail.com>
2018-12-30 17:25:10 +0100

    Throw test command

---
+++ Modules/throw.py
@@ -0,0 +1,9 @@
+﻿import Command
+
+@Command.CommandFunction()
+def Command(*Arguments):
+    print("I'm throwing now!")
+	raise SampleError("This is the message")
+    
+class SampleError(Exception):
+    pass
